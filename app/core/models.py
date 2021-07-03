274422dd3856fda92d730a4c35282531aa5345b9
Adil Reza <adilreza043@gmail.com>
2021-07-03 20:59:45 +0600

    checking for flak8

--- app/core/models.py
+++ app/core/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                             PermissionsMixin
 
-# Create your models here.
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
 
@@ -17,7 +16,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -11,12 +11,10 @@ class ModelTests(TestCase):
             email = email,
             password = password
         )
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
         email = 'adil@SELISE.CH'
         user = get_user_model().objects.create_user(email, 'useradil')
-
         self.assertEqual(user.email, email.lower())
