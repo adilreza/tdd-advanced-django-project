b4566aec37ffe3b212062a29ca650def7791c23e
Adil Reza <adilreza043@gmail.com>
2021-07-03 20:55:32 +0600

    modifying for flak8

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from app.calc import add
 
+
 class CalcTests(TestCase):
     def test_add_numbers(self):
         """ Test the two numbers are added"""
-        self.assertEqual(add(3,8), 11)
+        self.assertEqual(add(3, 8), 11)
